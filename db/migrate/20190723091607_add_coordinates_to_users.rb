03af327b28615933eb699dd68c45a41fe7d895a5
Arne Pedersen <arne.lamo@gmail.com>
2019-07-23 12:37:31 +0200

    added one more file

--- db/migrate/20190723091607_add_coordinates_to_users.rb
+++ db/migrate/20190723091607_add_coordinates_to_users.rb
@@ -1,4 +1,4 @@
-class AddCoordinatesToUsers < ActiveRecord::Migration[5.2]
+tclass AddCoordinatesToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :latitude, :float
     add_column :users, :longitude, :float
