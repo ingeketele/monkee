148d030e7fac39c39eda049adb730663b473066e
Inge Ketele <ingeketele@gmail.com>
2019-07-23 17:04:54 +0200

    add feature to add multiple pictures to new activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,6 +4,8 @@ class Activity < ApplicationRecord
   has_many :activity_reviews, through: :orders
   has_many :activity_categories, dependent: :destroy
   has_many :categories, through: :activity_categories
+  has_many :activity_images, dependent: :destroy
+  accepts_nested_attributes_for :activity_images
 
   validates :title, :address, :date, :description, :capacity, presence: true
 
