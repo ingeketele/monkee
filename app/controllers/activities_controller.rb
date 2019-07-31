f420ae16668f538cba98dda3737c1a38c308ee27
Manuel Velasco <velascome@gmail.com>
2019-07-31 14:52:25 +0200

    filters index

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,30 +3,47 @@ class ActivitiesController < ApplicationController
   before_action :set_categories, :set_age_group, only: [:index]
 
   def index
+    # ---- Created new variable with empty array -----------------
+    @cat_ages = []
+    @categories.each { |c| @cat_ages << c }
+    @age_group.each { |a| @cat_ages << a }
 
     @activities = Activity.all
     price_filter = params.dig(:sort, :price)
 
-    @activities = @activities.order(price_cents: :asc) if price_filter == "Lowest-to-Highest"
-    @activities = @activities.order(price_cents: :desc) if price_filter == "Highest-to-Lowest"
+    @activities = @activities.order(price_cents: :asc) if price_filter == "Lowest"
+    @activities = @activities.order(price_cents: :desc) if price_filter == "Highest"
     @actitites = @activities.sort_by { |activity| activity.favorites.size }.reverse.select { |a| a.favorites.any? } if params["popular"] == "true"
 
+    # ---- Search by -----------------
 
-    if params[:categories]
-      @activities = []
-      params[:categories].each do |category|
-        @activities << Activity.where(category: category)
-      end
-      @activities = @activities.flatten
+    if params.dig(:search_by, :name).present?
+      search = params.dig(:search_by, :name)
+      sql_query = "address ILIKE :name OR title ILIKE :name OR category ILIKE :name OR age_group ILIKE :name"
+      @activities = Activity.where(sql_query, name: "%#{search}%")
+    else
+      @activities = Activity.all if @activities.empty?
     end
 
-    if params[:age_group]
-      @activities = []
-      params[:age_group].each do |age_group|
-        @activities << Activity.where(age_group: age_group)
+    # ---- Merged categories and age_groups filtering -----------------
+
+    if params[:sort]
+      if params[:sort][:categories_age_groups]
+        parameters = params[:sort][:categories_age_groups].reject(&:empty?)
+        @activities = []
+        @cat = []
+        @agroup = []
+        parameters.each { |cag| @categories.include?(cag) ? @cat << cag : @agroup << cag }
+        @cat.each { |c| @activities << Activity.where(category: c) }
+        @activities = @activities.flatten
+        @agroup.each do |ag|
+          @activities = @activities.select { |activity| activity.age_group == ag }
+        end
+        @activities = Activity.all if @activities.empty?
       end
-      @activities = @activities.flatten
     end
+
+    # ---- MAP MARKERS -----------------
 
     @markers = @activities.map do |activity|
       if activity.latitude && activity.longitude
