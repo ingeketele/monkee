a18af67ab35c26b4746e2777e1e15e504c38d2c6
Inge Ketele <ingeketele@gmail.com>
2019-07-31 14:43:47 +0200

    updated navbar, cleaned styling on dashboard and newsfeed, edited styling on add activity form and added datepicker, also added slider for show page pictures

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,9 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
 import "bootstrap";
 
+import { initFlatpickr } from '../plugins/init_flatpickr';
 import { initMapbox } from '../plugins/init_mapbox';
 
 
+initFlatpickr();
 initMapbox();
-
